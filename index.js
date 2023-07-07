2ac7b244af322fa3f4d88c09fdccbe29fd270573
vfeofanov2000 <vfeofanov2000@gmail.com>
2023-07-07 18:55:28 +0300

    fix: correct mistaks

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 let burrgerBtn = document.querySelector('.burger-btn');
 let nav = document.querySelector('.nav');
+let header = document.querySelector('.header');
+
 burrgerBtn.addEventListener('click', () => {
     nav.classList.toggle('open');
+    header.classList.toggle('open');
 });
 
 let btns = nav.querySelectorAll('.nav-item');
 for (let btn of btns) {
     btn.addEventListener("click", () => {
         nav.classList.remove('open');
+        header.classList.remove('open');
     })
 }
